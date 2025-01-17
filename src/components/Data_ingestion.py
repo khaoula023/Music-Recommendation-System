3aeddf0ae59d889fc965450fb56e6ff36b494865
khaoula023 <khaouladahimi02@gmail.com>
2025-01-17 21:39:42 +0100

    Data Ingestion

--- src/components/Data_ingestion.py
+++ src/components/Data_ingestion.py
@@ -21,8 +21,8 @@ class DataIngestion:
             df = pd.read_csv('notebook\Data\data.csv')
             logging.info('Read the dataset as DataFrame')
             
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok= True)
-            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.data_path), exist_ok= True)
+            df.to_csv(self.ingestion_config.data_path, index=False, header=True)
             logging.info('Data ingestion is completed')
             return self.ingestion_config.data_path
         except Exception as e:
